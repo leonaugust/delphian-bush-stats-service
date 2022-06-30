371ebd43a18bad9045a8078038a469ed0349cd3a
Leon <leon-galushko@mail.ru>
2022-06-30 19:12:00 +0300

    Initial

---
+++ src/main/java/com/delphian/bush/StatsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.delphian.bush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatsServiceApplication.class, args);
+    }
+
+}
